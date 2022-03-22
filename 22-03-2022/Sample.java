72bf5991ebdf2c16af574cc49d8360c71a597ae3
Swetha2023b <97941758+Swetha2023b@users.noreply.github.com>
2022-03-22 13:03:38 +0530

    Add files via upload

---
+++ 22-03-2022/Sample.java
@@ -0,0 +1,15 @@
+public class Sample{
+	public static void main(String args[]){
+		//String string1="Swetha";
+		//String string2="Prakash";
+		//System.out.println(string1+" "+string2);
+		
+		int x=20;
+		int y=10;
+		int z=5;
+		System.out.println("Sum : " + (x+y+z));
+		System.out.println("Difference : " + (x-y-z));
+		System.out.println("Product : " + (x*y));
+		System.out.println("Quotient : " + (x/y));
+	}
+}
